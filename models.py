c3dfe338696eee11cb2698857093e1205d311824
MuhdSidi <muhdsidi07@gmail.com>
2026-03-18 22:59:30 +0100

    first commit

---
+++ models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+class Patient:
+
+    def __init__(self, name):
+        self.name = name
+        self.time = datetime.now()
+
+    def get_name(self):
+        return self.name
+
+    def get_time(self):
+        return self.time
