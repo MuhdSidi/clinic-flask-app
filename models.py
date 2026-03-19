f52120e7bfc045cf852d6daa908e09674e380c47
MuhdSidi <muhdsidi07@gmail.com>
2026-03-19 06:06:45 +0100

    Added comments to improve code clarity

--- models.py
+++ models.py
@@ -2,14 +2,13 @@
 from datetime import datetime
 
 class Patient:
-    # update 2
+    # update
     def __init__(self, name):
         self.name = name
         self.time = datetime.now()
-    # update 3
+    # update
     def get_name(self):
         return self.name
-    # update 4
-
+    # update
     def get_time(self):
         return self.time
