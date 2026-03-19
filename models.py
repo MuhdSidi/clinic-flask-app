27557dccaf102e627827815495fb42ee904664d6
MuhdSidi <muhdsidi07@gmail.com>
2026-03-19 06:11:57 +0100

    Final testing and debugging of application

--- models.py
+++ models.py
@@ -2,13 +2,16 @@
 from datetime import datetime
 
 class Patient:
+
     # update
     def __init__(self, name):
         self.name = name
         self.time = datetime.now()
+
     # update
     def get_name(self):
         return self.name
+    
     # update
     def get_time(self):
         return self.time
